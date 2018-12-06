3aebbf6e795ba1eef03b5a25cb6f62eaa802ab9a
SUSSEXDOWNS\476190 <476190@chrome.sussexdowns.ac.uk>
2018-12-06 14:30:59 +0000

    moer cards

---
+++ Assets/CardsModels.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CardModels : MonoBehaviour
+{
+    public Sprite[] faces;
+    public sprite cardback;
+
+    public int cardIndex; // e.g faces [card index];
+
+    public void ToggleFace(bool showFace)
+    {
+
+
+        if (showFace)
+        {
+            spriteRenderer.sprite = faces[cardIndex]
+        }
+        else
+        {
+            //TODO: Show the card face
+        }
+    }
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+}
