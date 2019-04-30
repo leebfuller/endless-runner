11525a5814695176127ac04af8900650b1a20ec8
SUSSEXDOWNS\476190 <476190@chrome.sussexdowns.ac.uk>
2019-04-30 12:08:16 +0100

    changed names of things and also moved things around

---
+++ Assets/script/CardStackView.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CardStack))]
+public class CardStackView : MonoBehaviour
+{
+    CardStack deck;
+
+    public Vector3 start;
+    public float cardOffset;
+    public GameObject cardPrefab;
+
+    void Start()
+    {
+        deck = GetComponent<CardStack>();
+        ShowCards();
+    }
+
+    void ShowCards()
+    {
+        int cardCount = 0;
+
+        foreach(int i in deck.GetCards())
+        {
+            float xPosition = cardOffset * cardCount;
+
+            GameObject cardCopy = (GameObject)Instantiate(cardPrefab);
+            Vector3 temp = start + new Vector3(xPosition, 0f);
+            cardCopy.transform.position = temp;
+
+            CardsModels cardsModels = cardCopy.GetComponent<CardsModels>();
+            cardsModels.cardIndex = i;
+            cardsModels.ToggleFace(true);
+
+            SpriteRenderer spriteRenderer = cardCopy.GetComponent<SpriteRenderer>();
+            spriteRenderer.sortingOrder = cardCount;
+
+            cardCount++;
+        }
+    }
+}
---
+++ Assets/script/DebugChangeCard.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugChangeCard : MonoBehaviour
+{
+    CardFlipper flipper;
+    CardsModels cardsModels;
+    int cardIndex = 0;
+
+    public GameObject card;
+
+    void Awake()
+    {
+        cardsModels = card.GetComponent<CardsModels>();
+        flipper = card.GetComponent<CardFlipper>();
+    }
+
+    void OnGUI()
+    {
+       if (GUI.Button(new Rect(10, 10, 100, 28), "Hit me!"))
+        {
+            if (cardIndex >= cardsModels.faces.Length)
+            {
+                cardIndex = 0;
+                flipper.FlipCard(cardsModels.faces[cardsModels.faces.Length - 1], cardsModels.cardBack, -1);
+            }
+            else
+            {
+                if (cardIndex  >0)
+                {
+                    flipper.FlipCard(cardsModels.faces[cardIndex - 1], cardsModels.faces[cardIndex], cardIndex);
+                }
+                else
+                {
+                    flipper.FlipCard(cardsModels.cardBack, cardsModels.faces[cardIndex], cardIndex);
+                }
+
+                cardIndex++;
+            }
+
+        }
+    }
+
+}
